63de141f272dd90b46b08c2ff04773551c0d6f5a
Илья <fetisov.ilya.andreevich@gmail.com>
2022-05-26 22:20:01 +0300

    Переделано

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,35 +2,40 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
-//        Task 1
 
-        int[] arr = new int[]{1, 2, 3};
-        double[] arr_2 = new double[]{1.123, 4.456, 3.88};
-        char[] arr_3= new char[]{'a', 'g', 'y'};
-//        for (int i = 0; i < arr.length; i++) {
-//            if (i == arr.length - 1) {
-//                System.out.print(arr[i] + " ");
-//            }
-//            else {
-//                System.out.print(arr[i] + ", ");
-//                }
-//            }
-//        for (int i = 0; i < arr_2.length; i++) {
-//            if (i == arr.length - 1) {
-//                System.out.print(arr_2[i] + " ");
-//            }
-//            else {
-//                System.out.print(arr_2[i] + ", ");
-//            }
-//        }
-//        for (int i = 0; i < arr.length; i++) {
-//            if (i == arr.length - 1) {
-//                System.out.print(arr_3[i] + " ");
-//            }
-//            else {
-//                System.out.print(arr_3[i] + ", ");
-//            }
-//        }
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        double[] arr2 = new double[3];
+        arr2[0] = 1.123;
+        arr2[1] = 3.55;
+        arr2[2] = 8.155;
+        char[] arr3 = new char[]{'a', 'g', 'y'};
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i] + "    ");
+            }
+            else {
+                System.out.print(arr[i] + ", ");
+                }
+            }
+        for (int i = 0; i < arr2.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr2[i] + "    ");
+            }
+            else {
+                System.out.print(arr2[i] + ", ");
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr3[i] + "    ");
+            }
+            else {
+                System.out.print(arr3[i] + ", ");
+            }
+        }
 
 
         for (int i = arr.length - 1; i > -1; i--) {
@@ -43,19 +48,35 @@ public class Main {
         }
         for (int i = arr.length - 1; i > -1; i--) {
             if (i != 0){
-                System.out.print(arr_2[i] + ", ");
+                System.out.print(arr2[i] + ", ");
             }
             else {
-                System.out.print(arr_2[i] + "     ");
+                System.out.print(arr2[i] + "     ");
             }
         }
         for (int i = arr.length - 1; i > -1; i--) {
             if (i != 0){
-                System.out.print(arr_3[i] + ", ");
+                System.out.print(arr3[i] + ", ");
             }
             else {
-                System.out.print(arr_3[i] + "    ");
+                System.out.print(arr3[i] + "    ");
             }
         }
+
+//        Task 4
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0){
+                arr[i] += 1;
+            }
+        }
+        for (int i = 0; i < arr3.length; i++) {
+            if (i < arr.length - 1){
+                System.out.print(arr[i] + ", ");
+            }
+            else {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
     }
 }
